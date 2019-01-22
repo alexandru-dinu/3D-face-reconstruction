a7060b2b4f40838551fa826a0fb11173a66714ba
Dan Muntean <dan_i.muntean@yahoo.com>
2019-01-22 14:08:47 +0200

    UNet model added

---
+++ src/u_net.py
@@ -0,0 +1,58 @@
+# full assembly of the sub-parts to form the complete net
+
+import torch.nn.functional as F
+import torch
+
+from u_net_parts import *
+
+class UNet(nn.Module):
+    def __init__(self, n_channels, n_classes):
+        super(UNet, self).__init__()
+        self.inc = DoubleConv(n_channels, 64)
+        self.down1 = Down(64, 128)
+        self.down2 = Down(128, 256)
+        self.down3 = Down(256, 512)
+        self.down4 = Down(512, 512)
+        self.up1 = Up(1024, 256)
+        self.up2 = Up(512, 128)
+        self.up3 = Up(256, 64)
+        self.up4 = Up(128, 64)
+
+        self.outc = torch.nn.Conv2d(
+            in_channels=64,
+            out_channels=200,
+            kernel_size=(1, 1),
+            stride=1,
+            padding=0
+        )
+
+    def forward(self, x):
+
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        import pdb;pdb.set_trace()
+        x = self.outc(x)
+
+        return x
+
+
+def to_cuda(data, use_cuda):
+    input_ = data.float()
+    if use_cuda:
+        input_ = input_.cuda()
+    return input_
+
+
+
+tensor = to_cuda(torch.ones(1, 1, 200, 200), True)
+nn = UNet(1, 10)
+nn.cuda()
+nn(tensor)
+n
---
+++ src/u_net_parts.py
@@ -0,0 +1,85 @@
+# sub-parts of the U-Net model
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class DoubleConv(nn.Module):
+    '''(conv => BN => ReLU) * 2'''
+
+    def __init__(self, in_ch, out_ch, padding=0):
+        super(DoubleConv, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(
+                in_channels=in_ch,
+                out_channels=out_ch,
+                kernel_size=(3, 3),
+                padding=padding),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(
+                in_channels=out_ch,
+                out_channels=out_ch,
+                kernel_size=(3, 3),
+                padding=padding),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        x = self.conv(x)
+        return x
+
+
+class Down(nn.Module):
+    def __init__(self, in_ch, out_ch, padding=0):
+        super(Down, self).__init__()
+        self.down_conv = nn.Sequential(
+            nn.MaxPool2d(
+                kernel_size=(2, 2),
+                stride=2
+            ),
+            DoubleConv(in_ch, out_ch, padding)
+        )
+
+    def forward(self, x):
+        x = self.down_conv(x)
+        return x
+
+
+class Up(nn.Module):
+    def __init__(self, in_ch, out_ch, bilinear=True, padding=0):
+        super(Up, self).__init__()
+
+        #  would be a nice idea if the upsampling could be learned too,
+        #  but my machine do not have enough memory to handle all those weights
+        if bilinear:
+            self.up = nn.Upsample(scale_factor=2, mode='bilinear')
+        else:
+            self.up = nn.ConvTranspose2d(
+                in_channels=in_ch // 2,
+                out_channels=in_ch // 2,
+                stride=2)
+
+        self.conv = DoubleConv(in_ch, out_ch, padding)
+
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+
+        # input is CHW
+        diffY = x2.size()[2] - x1.size()[2]
+        diffX = x2.size()[3] - x1.size()[3]
+
+        x1 = F.pad(x1, (diffX // 2, diffX - diffX // 2,
+                        diffY // 2, diffY - diffY // 2))
+
+        # for padding issues, see
+        # https://github.com/HaiyongJiang/U-Net-Pytorch-Unstructured-Buggy/commit/0e854509c2cea854e247a9c615f175f76fbb2e3a
+        # https://github.com/xiaopeng-liao/Pytorch-UNet/commit/8ebac70e633bac59fc22bb5195e513d5832fb3bd
+
+        x = torch.cat([x2, x1], dim=1)
+        x = self.conv(x)
+        return x
+
+
